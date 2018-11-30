85bb02a76135bb9b3e778cc5d28ceb48d004a1b4
jarod <jrwhite20@gmail.com>
2018-11-30 00:23:58 -0600

    cleaned up

--- app/actions/ghostSynapse.ts
+++ app/actions/ghostSynapse.ts
@@ -1,18 +1,18 @@
-import { actionCreator, actionCreatorVoid } from "./helpers";
-export interface MakeGhostSynapseAtAxonAction {
-  id: string;
-  neuronId: string;
+import { actionCreator, actionCreatorVoid } from './helpers'
+export interface MakeGhostAtAxonAction {
+  id: string
+  neuronId: string
 }
 
-export interface MakeGhostSynapseAtDendAction {
-  id: string;
-  neuronId: string;
+export interface MakeGhostAtDendAction {
+  id: string
+  neuronId: string
 }
-export const makeGhostSynapseAtAxon = actionCreator<
-  MakeGhostSynapseAtAxonAction
->("MAKE_GHOST_SYNAPSE_AT_AXON");
-export const makeGhostSynapseAtDend = actionCreator<
-  MakeGhostSynapseAtDendAction
->("MAKE_GHOST_SYNAPSE_AT_DEND");
+export const makeGhostSynapseAtAxon = actionCreator<MakeGhostAtAxonAction>(
+  'MAKE_GHOST_SYNAPSE_AT_AXON'
+)
+export const makeGhostSynapseAtDend = actionCreator<MakeGhostAtDendAction>(
+  'MAKE_GHOST_SYNAPSE_AT_DEND'
+)
 
-export const resetGhostSynapse = actionCreatorVoid("RESET_GHOST_SYNAPSE");
+export const resetGhostSynapse = actionCreatorVoid('RESET_GHOST_SYNAPSE')
